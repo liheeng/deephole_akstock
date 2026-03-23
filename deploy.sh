e52bb73570653ee2d2189f3713f55af259f0eb98
Heng Li <liheeng@gmail.com>
2026-03-23 19:03:34 +0800

    upgrade build scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
-docker build -t akshare-stock:1.1 .
+#!/bin/bash
+
+set -o allexport
+source .env.dev
+set +o allexport
+
+docker build -t akshare-stock:"${IMAGE_VERSION}" .
 docker compose --env-file .env.dev up akstock_db_init
 docker compose --env-file .env.dev up -d akstock_stock_fetcher
 docker compose --env-file .env.dev up -d akstock_api_service
--- undeploy.sh
+++ undeploy.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+set -o allexport
+source .env.dev
+set +o allexport
+
 docker compose --env-file .env.dev down
-docker rmi akshare-stock:1.1
+docker rmi akshare-stock:"${IMAGE_VERSION}"
